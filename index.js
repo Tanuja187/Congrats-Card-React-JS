09af102f72a25fae1bf8ce387f6a1380ffc516c7
Tanuja <tanujachari@gmail.com>
2023-06-26 16:14:32 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div class="container">
+    <h1 class="heading">Congratulations</h1>
+    <div class="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 class="name">Kiran V</h1>
+      <p class="para">
+        Vishnu Institution of Computer Education and Technology. Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
